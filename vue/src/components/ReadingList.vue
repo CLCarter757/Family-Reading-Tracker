bdfe4713aeb406e4b63d8544df916bbf247eec29
Clint Carter <CLCarter757@gmail.com>
2022-04-13 11:48:14 -0400

    The Vue

---
+++ vue/src/components/ReadingList.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <h2>Reading List</h2>
+        <div class="reading-list">
+            <book-card :book="book" :key="book.isbn"/>
+        </div>
+    </section>
+</template>
+
+<script>
+import BookCard from '@/components/BookCard.vue';
+
+export default {
+    name:"reading-list",
+    components: {
+        BookCard
+    }
+}
+</script>
+
+<style>
+.best-seller-list {
+    display:flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+</style>
